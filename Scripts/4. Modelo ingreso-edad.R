05d138631854a75e8a268de7ae39c32b69212505
Juliet-Molano <j.molanorizo@uniandes.edu.co>
2024-09-14 18:02:10 -0500

    modelo

--- Scripts/4. Modelo ingreso-edad.R
+++ Scripts/4. Modelo ingreso-edad.R
@@ -31,138 +31,104 @@ data_webs <- import(file = "base_final.rds")
         
  #2. Definir los posibles predictores de la base de datos---------------------- 
 
- #i. Regresión: Log(wage)=b1 + b2(age) + b3(age)^2 + u (sin controles)
-  model_Age_wage <- lm(log_ing_h_win ~ Edad_win + Edad2, data = data_webs) #Realizamos la regresión
-  summary(model_Age_wage)
-  stargazer(model_Age_wage, type = "text") # Modelo simple
-  
-#ii. Regresión: Log(wage)=b1 + b2(age) + b3(age)^2 + u (con controles)
-  model_Age_wage_cont1 <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + Tamanio_empresa + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win , data = data_webs) #Realizamos la regresión
-  summary(model_Age_wage_cont1)
-  stargazer(model_Age_wage_cont1, type = "text") # Modelo con controles
+   #i. Regresión: Log(wage)=b1 + b2(age) + b3(age)^2 + u (sin controles)
+    model_Age_wage <- lm(log_ing_h_win ~ Edad_win + Edad2, data = data_webs) #Realizamos la regresión
+    summary(model_Age_wage)
+    stargazer(model_Age_wage, type = "text") # Modelo simple
+    
+  #ii. Regresión: Log(wage)=b1 + b2(age) + b3(age)^2 + u (con controles)
+    model_Age_wage_cont1 <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + tfirma_factor + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win , data = data_webs) #Realizamos la regresión
+    summary(model_Age_wage_cont1)
+    stargazer(model_Age_wage_cont1, type = "text") # Modelo con controles
 
         # Generar tablas de regresion - modelos indiciales con todas las observaciones
         setwd(paste0(wd,"/Latex"))
         stargazer(model_Age_wage, type = "text", keep = c("Edad", "Edad2") ) #Modelo completo
         stargazer(model_Age_wage, model_Age_wage_cont1,
                   keep = c("Edad", "Edad2"),
-                  dep.var.caption  = "Logaritmo del salario",
+                  align = TRUE, 
+                  dep.var.labels.include = FALSE,
+                  dep.var.caption  = "Log(Ingresos por hora)",            
                   column.labels   = c("Sin controles", "Con controles"),
                   covariate.labels = c("Edad", "Edad al cuadrado"),
                   type="latex",out = "modelos_edad_ingresos.tex")
         
-        # Leer el contenido del archivo
+        # Leer el contenido del archivo y modificar
         regression_table <- readLines("modelos_edad_ingresos.tex")
         regression_table <- gsub("Edad_win", "Edad", regression_table) #Reemplazar "Observations" con "Observaciones"
         regression_table <- gsub("Observations", "Observaciones", regression_table) #Reemplazar "Observations" con "Observaciones"
         regression_table <- gsub("Adjusted R$^{2}$ ", "R$^{2}$ ajustado", regression_table)
         writeLines(regression_table, "modelos_edad_ingresos.tex") #Escribir el contenido modificado de nuevo al archivo
         
-        
   #3. Analisis de observaciones influyentes ------------------------------------
   
-  ## leverage 
-  data_webs <- data_webs %>% 
-    ungroup() %>%
-    mutate(leverage = hatvalues(model_Age_wage_cont1))  # Calcula los valores leverage
-  length(hatvalues(model_Age_wage_cont1))
+    #i. Eliminar observaciones con alto apalancamiento
+              
+        ## leverage 
+        data_webs <- data_webs %>% 
+          ungroup() %>%
+          mutate(leverage = hatvalues(model_Age_wage_cont1))  # Calcula los valores leverage
+        length(hatvalues(model_Age_wage_cont1))
+        
+        ## residuals
+        data_webs <- data_webs %>%
+          ungroup() %>%
+          mutate(residuals = residuals(model_Age_wage_cont1))  
+        length(residuals(model_Age_wage_cont1))
+        
+        #Calcula el apalancamiento medio
+        p <- mean(data_webs$leverage)
+        p
+        
+        #punto de corte
+        cutt <- 3*p
+        cutt
+        
+        #dejar las observaciones que se consideran influyentes
+        data_webs2 <- data_webs %>%
+          filter(leverage <= cutt)
   
-  ## residuals
-  data_webs <- data_webs %>%
-    ungroup() %>%
-    mutate(residuals = residuals(model_Age_wage_cont1))  
-  length(residuals(model_Age_wage_cont1))
-  
-  
-  {
-  N <- nrow(data_webs)
-  data_webs$id <- seq(1 , N)
+
+    #ii .Correr nodelo sin observaciones influyentes
+    n_model_Age_wage_cont1 <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + tfirma_factor + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win + Experiencia_win, data = data_webs2) #Realizamos la regresión
+    summary(n_model_Age_wage_cont1)
+    stargazer(n_model_Age_wage_cont1, type = "text") # Modelo con controles
     
-  a<- ggplot(data_webs , aes(y = leverage , x = id , color= Edad , shape= as.factor(Sexo) )) +
-    geom_point() + # add points
-    theme_bw() + #black and white theme
-    labs(x = "Observaciones",  
-         y = "Leverage",
-         title = "") # labels
-  a
-  
-  b<- ggplot(data_webs , aes(y = leverage , x = residuals )) +
-    geom_point() + # add points
-    theme_bw() + #black and white theme
-    labs(x = "Residuales",  
-         y = "Leverage",
-         title = "") # labels
-  b
-  
-  # Arrange the ggplot2 plots side by side using grid.arrange()
-  grid.arrange(a, b, ncol = 2)
-  }
+    data_webs2 <- data_webs2 %>%
+      ungroup() %>%
+      mutate(residuals2 = residuals(n_model_Age_wage_cont1))  
+    length(residuals(n_model_Age_wage_cont1))
+    
 
-  
-  
-  
-  #Calcula el apalancamiento medio
-  p <- mean(data_webs$leverage)
-  p
-  
-  #punto de corte
-  cutt <- 3*p
-  cutt
-  
-  #dejar las observaciones que se consideran influyentes
-  data_webs2 <- data_webs %>%
-    filter(leverage <= cutt)
-  
-  #---- Correr nuevamente el modelo -------#
-
-  #Modelo de prueba sin observaciones influyentes
-  n_model_Age_wage_cont1 <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + Tamanio_empresa + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win + Experiencia_win, data = data_webs2) #Realizamos la regresión
-  summary(n_model_Age_wage_cont1)
-  stargazer(n_model_Age_wage_cont1, type = "text") # Modelo con controles
-  
-  data_webs2 <- data_webs2 %>%
-    ungroup() %>%
-    mutate(residuals2 = residuals(n_model_Age_wage_cont1))  
-  length(residuals(n_model_Age_wage_cont1))
-  
-  #Grafica residuos del modelo
-  residuos <- ggplot(data = data_webs2, 
-                     mapping = aes(x=residuals2)) +
-    theme_bw() + 
-    geom_density()
-  png("residuos_sin_var_influy") # Formato grafica
-  residuos
-  dev.off() # Cierra la grafica
   
   #4. Analisis de valores atipicos del modelo-----------------------------------
+    
+    #i. Grafucas de los residuos 
+    residuos_c <- ggplot(data = data_webs, 
+                       mapping = aes(x=residuals)) +
+      theme_bw() + 
+      geom_density() +
+      labs(title = "",
+           x = "Resuduos",
+           y = "Densidad") 
+    png("residuos_sin_var_influy") # Formato grafica
+    residuos_c
+    dev.off() # Cierra la grafica
   
-  data_webs <-data_webs %>% mutate(m1_std_residuals_c = studres(model_Age_wage_cont1))
-  
-  ggplot(data_webs , aes(y = m1_std_residuals_c, x = id , color= Edad_win, shape= as.factor(Mujer) )) +
-    geom_point() + # add points
-    theme_bw() + #black and white theme
-    labs(x = "Observaciones",  
-         y = "Residuos",
-         title = "") # labels
-  
-  ##Residuo estudentizado
-  data_webs <-data_webs %>% mutate(m1_std_residuals_c = studres(model_Age_wage_cont1))
 
-  ggplot(data_webs , aes(y = m1_std_residuals_c , x = id , color= Edad_win, shape= as.factor(Mujer) )) +
-    geom_point() + # add points
-    theme_bw() + #black and white theme
-    labs(x = "Observaciones",  
-         y = "Residuos",
-         title = "") # labels
+    #ii. Residuo estudentizado
+    data_webs <-data_webs %>% mutate(m1_std_residuals_c = studres(model_Age_wage_cont1))
+
+    #iii. Eliminacion de valores atipicos
+    data_webs3 <- data_webs %>% filter(m1_std_residuals_c<2 & m1_std_residuals_c>-2 )
   
-  #Eliminacion de valores atipicos
-  data_webs3 <- data_webs %>% filter(m1_std_residuals_c<2 & m1_std_residuals_c>-2 )
-  
-  # Correr nuevo the model
-  model_Age_wage_cont1_vt <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + Tamanio_empresa + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win + Experiencia_win, data = data_webs3) #Realizamos la regresión
-  summary(model_Age_wage_cont1_vt)
-  stargazer(model_Age_wage_cont1_vt, type = "text") # Modelo con controles
-  
+    #iv.  Correr nuevo the model
+    model_Age_wage_cont1_vt <- lm(log_ing_h_win ~ Edad_win + Edad2 + Mujer + estrato_factor + dummy_jefe + edu_factor + Tamanio_empresa + Trabajo_informal + Independiente + oficio_factor  + Horas_trabajadas_win + Experiencia_win, data = data_webs3) #Realizamos la regresión
+    summary(model_Age_wage_cont1_vt)
+    stargazer(model_Age_wage_cont1_vt, type = "text") # Modelo con controles
+    
+  #5. Comparacion modelos con controles analisis-----------------------------------
+    
 
   #Tabla de modelo con controles
   stargazer(model_Age_wage_cont1,n_model_Age_wage_cont1,model_Age_wage_cont1_vt,
@@ -183,27 +149,8 @@ data_webs <- import(file = "base_final.rds")
   regression_table <- gsub("Adjusted R$^{2}$ ", "R$^{2}$ ajustado", regression_table)
   writeLines(regression_table, "modelos_edad_ingresos_controles.tex") #Escribir el contenido modificado de nuevo al archivo
   
-  #Tabla de modelos finales
-  stargazer(model_Age_wage, model_Age_wage_cont1_vt,
-            keep = c("Edad_win", "Edad2"),
-            dep.var.caption  = "",
-            title = "",
-            align = TRUE,
-            header = FALSE,
-            dep.var.labels.include = FALSE,
-            column.labels   = c("Log(ingreso por hora)", "Log(ingreso por hora)"),
-            covariate.labels = c("Edad", "Edad al cuadrado"),
-            type="latex",
-            out = "modelos_edad_ingresos_final.tex")  
-  
-  # Leer el contenido del archivo
-  regression_table <- readLines("modelos_edad_ingresos_final.tex")
-  regression_table <- gsub("Observations", "Observaciones", regression_table) #Reemplazar "Observations" con "Observaciones"
-  regression_table <- gsub("Adjusted R$^{2}$ ", "R$^{2}$ ajustado", regression_table)
-  writeLines(regression_table, "modelos_edad_ingresos_final.tex") #Escribir el contenido modificado de nuevo al archivo
-  
-  
-  #Modelo para colocar en el documento ----------------------------------------
+ 
+  #6. Modelo para colocar en el documento ----------------------------------------
   
   stargazer(model_Age_wage, model_Age_wage_cont1,model_Age_wage_cont1_vt,
             keep = c("Edad_win", "Edad2"),
@@ -221,25 +168,10 @@ data_webs <- import(file = "base_final.rds")
   regression_table <- gsub("Adjusted R$^{2}$ ", "R$^{2}$ ajustado", regression_table)
   writeLines(regression_table, "modelos_edad_ingreso_table.tex") #Escribir el contenido modificado de nuevo al archivo
 
-  
-  #Tabla de modelos finales
-  stargazer(model_Age_wage, model_Age_wage_cont1_vt,
-            keep = c("Edad_win", "Edad2"),
-            dep.var.caption  = "",
-            title = "",
-            align = TRUE,
-            header = FALSE,
-            dep.var.labels.include = FALSE,
-            column.labels   = c("Log(ingreso por hora)", "Log(ingreso por hora)"),
-            covariate.labels = c("Edad", "Edad al cuadrado"),
-            type="latex",
-            out = "modelos_edad_ingresos_final.tex")  
-  
-  
-  
+
   #5. Valor maximo -----------------------------------------------------------
   
-  #i. MODELO SIN CONTROLES
+  #i. Modelo sin controles
   
   #-- Forma 1 --#
   
